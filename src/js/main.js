9cf4de64a0f5cac01f1c8d273cda6f45f60c764b
gamejoan <39127073+gamejoan@users.noreply.github.com>
2022-11-29 11:59:30 -0600

    Se agrega Proyecto

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import UpdateCountDown from "./countdown";
+import { loader } from "./forms";
+import "./numberInput";
+
+// init count Down
+UpdateCountDown("Jan 14, 2023 13:00:00");
+
+setTimeout(() => {
+  loader.classList.add("hidden");
+}, 1000);
+
+// target elements
+
+// forms
+
+// timer
+
+// animation
+
+// guests icons
+
+// timer
+
+// forms SubmitEvent. event defautl
+
+// gust
